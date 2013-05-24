f979dbc1a386d5000e7de9aa516532cc579ed1e8
Michael Giles <mikegiles58@yahoo.com>
2013-05-23 17:13:37 -0700

    Add update socket demo files

--- server.js
+++ server.js
@@ -1,35 +1,44 @@
+// Expose required mod objects
 var io = require('socket.io'), connect = require('connect');
 
-var app = connect().use(connect.static('public')).listen(3000);
-
+// Setup server
+var app = connect().use(connect.static('public')).listen(8080);
 var server = io.listen(app);
 
-	server.sockets.on('connection', function (socket) {
-  
-	  	socket.emit('entrance', {message: 'Socket is connected'});
+// Socket events
+server.sockets.on('connection', function (socket) {
+	
+	// Session Start Event
+	socket.on('sessionStart', function (data) {
+						
+		server.sockets.emit('sessionStart', {message: 'session started' });
+ 
+    }); 
+	
+	// Session End Event
+	socket.on('sessionEnd', function (data) {
+						
+		server.sockets.emit('sessionEnd', {message: 'session ended'});
+ 
+    });
+    
+    // Message Event
+	socket.on('message', function (msg) {
+						
+		console.log('Message: ', msg);
+		
+		server.sockets.emit('message', msg );
+ 
+    });
+    
+	// Message Event
+	socket.on('modeChange', function (msg) {
+		
+		console.log('I received a modeChange event ', msg);
 
-		socket.on('disconnect', function  () {
-			server.sockets.emit('exit', {message: 'A user has disconnected.'});
-		});
-		
-		socket.on('chat', function  (data) {
-			server.sockets.emit('chat', {message: '# ' + data.message});
-		});
-		
-		// Test custom event 
-		socket.on('testEvent', function  (data) {
-			server.sockets.emit('testEvent', {message: '# ' + data.message});
-		});
-		
-		socket.on('private message', function (from, msg) {
-			console.log('I received a private message by ', from, ' saying ', msg);
-		});
-		
-		socket.on('event', function  (data) {
-			server.sockets.emit('event', {message: '# ' + data.message});
-		});
-		
+		server.sockets.emit('modeChange', msg);
+		 
+    });	
+	
 
-		server.sockets.emit('entrance', {message: 'New user is connected'});
-
-	}); // end ready 
+}); // end ready 
